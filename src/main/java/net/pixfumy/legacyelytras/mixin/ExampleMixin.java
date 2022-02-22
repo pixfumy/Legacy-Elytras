0ea0824b38e472ede696e66d197e2bebe6e3f916
pixfumy <asaddurrani1@live.ca>
2022-02-21 21:32:37 -0500

    add basic logic for starting flight

--- src/main/java/net/pixfumy/legacyelytras/mixin/ExampleMixin.java
+++ src/main/java/net/pixfumy/legacyelytras/mixin/ExampleMixin.java
@@ -1,15 +1,35 @@
 package net.pixfumy.legacyelytras.mixin;
 
-import net.minecraft.client.gui.screen.TitleScreen;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.input.Input;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!");
+@Mixin(ClientPlayerEntity.class)
+public class ExampleMixin extends AbstractClientPlayerEntity {
+	@Shadow public Input input;
+	private int ticksSinceOnGround = 0;
+
+	public ExampleMixin(World world, GameProfile gameProfile) {
+		super(world, gameProfile);
+	}
+
+	@Inject(at = @At("HEAD"), method = "tickMovement")
+	private void attemptStartFallFlying(CallbackInfo ci) {
+		if (this.onGround) {
+			this.ticksSinceOnGround = 0;
+		} else {
+			this.ticksSinceOnGround++;
+		}
+		if (this.input.jumping && this.ticksSinceOnGround > 4) {
+			System.out.println("icarus");
+		}
 	}
 }
