927fed65676832201041e9debbc0b9ff78e34f96
pixfumy <asaddurrani1@live.ca>
2022-02-24 16:37:20 -0500

    fix flying logic, elytra must now be equipped

--- src/main/java/net/pixfumy/legacyelytras/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/net/pixfumy/legacyelytras/mixin/ClientPlayerEntityMixin.java
@@ -4,7 +4,10 @@ import com.mojang.authlib.GameProfile;
 import net.minecraft.client.input.Input;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import net.pixfumy.legacyelytras.IPlayerEntity;
+import net.pixfumy.legacyelytras.ItemElytra;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,9 +15,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayerEntity.class)
-public class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
+public class ClientPlayerEntityMixin extends AbstractClientPlayerEntity implements IPlayerEntity {
 	@Shadow public Input input;
 	public boolean isFlying;
+	public boolean lastJumping = false;
 	private int ticksSinceOnGround = 0;
 
 	public ClientPlayerEntityMixin(World world, GameProfile gameProfile) {
@@ -28,9 +32,34 @@ public class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
 		} else {
 			this.ticksSinceOnGround++;
 		}
-		if (this.input.jumping && this.ticksSinceOnGround > 4) {
-			System.out.println("icarus");
+		if (this.input.jumping) {
+			boolean hasUsableElytra = false;
+			ItemStack chest = this.getArmorStacks()[2];
+			if (chest != null && chest.getItem() instanceof ItemElytra && chest.getDamage() < chest.getMaxDamage()) {
+				hasUsableElytra = true;
+			}
+			if (!this.lastJumping && this.ticksSinceOnGround > 3 && !this.isTouchingWater() &&
+					!this.isClimbing() && !this.hasVehicle() && hasUsableElytra) {
+				this.startFallFlying();
+			}
+			this.lastJumping = true;
+		} else {
+			this.lastJumping = false;
 		}
 	}
-	//TODO: account for water and for holding space
+
+	@Override
+	public boolean isFallFlying() {
+		return this.isFlying;
+	}
+
+	@Override
+	public void startFallFlying() {
+		this.isFlying = true;
+	}
+
+	@Override
+	public void stopFallFlying() {
+		this.isFlying = false;
+	}
 }
